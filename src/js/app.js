0e32358676fdc8ca3c3e148303d2a4dce4fa8776
Maxim Axenov <topcmc32@gmail.com>
2022-09-14 22:09:01 +0500

    commited second On branch master Your branch is up to date with 'origin/master'. 	modified:   src/js/app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,6 @@
-export default function specialAttack(obj) {
-  for (let i = 0; i < obj.special.length; i++) {
-    if (!('description' in obj.special[i])) {
-      obj.special[i].description = 'Описание недоступно';
-    }
-  }
-  return [...obj.special];
+export default function specialAttack({ special }) {
+  return special.map((attack) => ({
+    description: 'Описание недоступно',
+    ...attack,
+  }));
 }
